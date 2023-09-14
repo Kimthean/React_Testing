4e4eb277bf276fb67fbb0bdbf59ca81fc8268710
Thean089 <112529779+Thean089@users.noreply.github.com>
2023-09-14 16:28:59 +0700

    changed menu icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,21 @@ import { Link } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// Define the ToggleIcon component with the custom SVG
+function ToggleIcon() {
+	return (
+		<svg
+			width="18"
+			height="14"
+			viewBox="0 0 18 14"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path d="M0 0V2H18V0H0ZM0 8H18V6H0V8ZM0 14H18V12H0V14Z" fill="white" />
+		</svg>
+	);
+}
+
 function Header() {
 	return (
 		<div className="container">
@@ -17,7 +32,10 @@ function Header() {
 							</div>
 						</Navbar.Brand>
 
-						<Navbar.Toggle aria-controls="basic-navbar-nav" />
+						{/* Place the custom ToggleIcon within the Navbar.Toggle */}
+						<Navbar.Toggle aria-controls="basic-navbar-nav">
+							<ToggleIcon />
+						</Navbar.Toggle>
 
 						<Navbar.Collapse id="main-navigation">
 							<Nav className="menu">
